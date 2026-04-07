6b55a9e89c3fb9c49d0ba03be57399f45bcb9c3d
Archie_nn <yangenqi805@gmail.com>
2026-04-07 13:44:18 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace Personal_mortgage_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(49, 33);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(545, 248);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "輸入";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox10);
+            this.groupBox2.Controls.Add(this.textBox9);
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox2.Location = new System.Drawing.Point(49, 304);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(545, 253);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "輸出";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "房屋總價(Total House Price)：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(224, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "自備款比例(Down Payment %) :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(234, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "貸款利率(Annual Interest Rate)：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(168, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "貸款年限(Loan Term)：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 203);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(206, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "寬限期(Grace Period, 選填)：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(30, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 19);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "貸款總金額:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(30, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(157, 19);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "每月應繳金額(本+ 息):";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(30, 124);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 19);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "首期利息與首期本金:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(30, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(87, 19);
+            this.label9.TabIndex = 4;
+            this.label9.Text = "總利息支出:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(30, 201);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(87, 19);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "總還款金額:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.textBox1.Location = new System.Drawing.Point(273, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(131, 27);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(273, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(131, 27);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(273, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(131, 27);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(273, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(131, 27);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(273, 203);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(131, 27);
+            this.textBox5.TabIndex = 9;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(273, 43);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(131, 27);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(273, 80);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(131, 27);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(273, 121);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(131, 27);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(273, 162);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(131, 27);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(273, 198);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(131, 27);
+            this.textBox10.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(418, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 80);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 573);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Personal_mortgage_calculator
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [System.Runtime.InteropServices.DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        private void HighlightResults()
+        {
+            // 貸款總額用淺藍色，總還款用淺綠色
+            textBox6.BackColor = Color.AliceBlue;
+            textBox10.BackColor = Color.Honeydew;
+
+            // 讓文字靠右對齊 (符合金融習慣)
+            foreach (var tb in new[] { textBox6, textBox7, textBox8, textBox9, textBox10 })
+            {
+                tb.TextAlign = HorizontalAlignment.Right;
+                tb.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+            }
+        }
+        private void SetModernStyle(Control ctrl)
+        {
+            ctrl.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, ctrl.Width, ctrl.Height, 15, 15));
+        }
+
+        private void RefreshUI()
+        {
+            // 調用美化
+            SetModernStyle(button1);
+            HighlightResults();
+
+            // 如果總利息超過貸款本金的一半，將總利息文字變紅 (警示)
+            if (double.Parse(textBox9.Text) > (double.Parse(textBox6.Text) / 2))
+            {
+                textBox9.ForeColor = Color.Red;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool is1Valid = double.TryParse(textBox1.Text, out double txt1);
+            bool is2Valid = double.TryParse(textBox2.Text, out double txt2);
+            bool is3Valid = double.TryParse(textBox3.Text, out double txt3);
+            bool is4Valid = double.TryParse(textBox4.Text, out double txt4);
+            bool is5Valid = double.TryParse(textBox5.Text, out double txt5);
+
+            // 驗證身高輸入
+            if (is1Valid)
+            {
+                if (txt1 <= 0)
+                {
+                    MessageBox.Show("房屋總價(Total House Price)必須大於0", "房屋總價(Total House Price)錯誤",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的房屋總價(Total House Price)", "房屋總價(Total House Price)錯誤",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // 驗證體重輸入
+            if (is2Valid)
+            {
+                if (txt2 <= 0 || txt2 > 100)
+                {
+                    MessageBox.Show("自備款比例(Down Payment %)單位為百分比", "自備款比例(Down Payment %)錯誤",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的自備款比例(Down Payment %)", "自備款比例(Down Payment %)錯誤",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (is3Valid)
+            {
+                if (txt3 <= 0 || txt3 > 100)
+                {
+                    MessageBox.Show("貸款利率(Annual Interest Rate)單位為百分比", "貸款利率(Annual Interest Rate)錯誤",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的貸款利率(Annual Interest Rate)", "貸款利率(Annual Interest Rate)錯誤",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // 驗證體重輸入
+            if (is4Valid)
+            {
+                if (txt4 <= 0)
+                {
+                    MessageBox.Show("貸款年限(Loan Term)必須大於零", "貸款年限(Loan Term)錯誤",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的貸款年限(Loan Term)", "貸款年限(Loan Term)錯誤",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            if (is5Valid)
+            {
+                if (txt5 < 0)
+                {
+                    MessageBox.Show("寬限期(Grace Period)必須大於零。", "寬限期(Grace Period)錯誤",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入有效的寬限期(Grace Period)", "寬限期(Grace Period)錯誤",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            double h = double.Parse(textBox1.Text);
+            double p = double.Parse(textBox2.Text);
+            double a = double.Parse(textBox3.Text);
+            double l = double.Parse(textBox4.Text);
+            double g = double.Parse(textBox5.Text);
+            double P = h * (1 - p * 0.01); // 貸款總額
+            double r = a / 12 / 100;               // 月利率
+            double n = l * 12;                         // 總期數
+            double G = g * 12;                        // 寬限期期數
+
+            // 1. 每月應繳 (以寬限期後為準)
+            double monthlyPayment;
+            if (n == g) // 極端情況：全寬限期
+                monthlyPayment = P * r;
+            else
+                monthlyPayment = P * (r * Math.Pow(1 + r, n - g)) / (Math.Pow(1 + r, n - g) - 1);
+
+            // 2. 首期利息與本金
+            double firstMonthInterest = P * r;
+            double firstMonthPrincipal = (g > 0) ? 0 : monthlyPayment - firstMonthInterest;
+
+            // 3. 總利息
+            double totalInterest = (P * r * g) + (monthlyPayment * (n - g)) - P;
+
+            // 4. 總還款額
+            double totalAmount = P + totalInterest;
+
+            textBox6.Text = P.ToString("N2");              // 貸款總額
+            textBox7.Text = monthlyPayment.ToString("N2");   // 每月應繳
+            textBox8.Text = firstMonthInterest.ToString("N2"); // 首期利息
+            textBox9.Text = totalInterest.ToString("N2");    // 總利息
+            textBox10.Text = totalAmount.ToString("N2");      // 總還款額
+
+            RefreshUI();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
